69f7587ade88b9859bb0292debe668b6c80ef1c9
bhuumika <40338615+bhuumika@users.noreply.github.com>
2018-06-26 18:39:14 +0530

    Add files via upload

---
+++ Grid_Gate.java
@@ -0,0 +1,18 @@
+package com.qait.automation.Tatoc_With_PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Grid_Gate {
+	WebDriver driver;
+	public Grid_Gate(WebDriver driver) {
+		this.driver=driver;
+	
+	}
+	public void grid()
+	{
+		driver.findElement(By.className("greenbox")).click();
+	}
+	
+
+}
